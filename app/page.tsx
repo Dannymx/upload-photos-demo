dec149d6f45438eb925d42199b9ea951ca8415cb
Dan Saldana <hello@dan.mx>
2023-10-11 01:09:54 -0500

    feat: add label as file input (for styling)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,43 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import type { FormEvent } from "react";
+
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
+  const handleUpload = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <main className="container flex min-h-screen flex-col gap-8 p-24">
+    <main className="container flex min-h-screen max-w-screen-md flex-col gap-8 p-8">
       <div className="flex flex-row gap-4">
         <div className="w-1/2 grow">
           <Input className="p-6" type="text" placeholder="Search Images..." />
         </div>
         <div className="flex items-center justify-end">
-          <Button className="h-full px-10 text-xl">Upload</Button>
+          <form
+            className="h-full"
+            action="/api/photos"
+            method="post"
+            onSubmit={(e) => handleUpload(e)}
+          >
+            <Label
+              className="h-full px-10 text-xl"
+              htmlFor="fileUpload"
+              variant="asButton"
+            >
+              Upload
+              <Input
+                className="hidden"
+                type="file"
+                name="photos"
+                id="fileUpload"
+              />
+            </Label>
+          </form>
         </div>
       </div>
       <div className="flex flex-col gap-4">
---
+++ components/ui/label.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  {
+    variants: {
+      variant: {
+        asButton:
+          "inline-flex items-center justify-center rounded-md bg-primary text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+  },
+);
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants>
+>(({ className, variant, size, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants({ variant, size, className }))}
+    {...props}
+  />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
